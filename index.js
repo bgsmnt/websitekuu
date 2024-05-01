ba2886e0ae5cc792093b2df9e6f11c2fe68fe3fb
manutommmmmmmmmmmmmmmmmm <134299193+manutommmmmmmmmmmmmmmmmm@users.noreply.github.com>
2024-05-01 12:01:14 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function redirInstagram() {
+    window.location.href = "https://instagram.com/bgs.panpan"
+}
+function redirWhatsApp() {
+    window.location.href = "https://wa.me/+6287730223619"
+}
